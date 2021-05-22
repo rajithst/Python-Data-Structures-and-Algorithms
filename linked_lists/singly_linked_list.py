671ec7d98c5076999f0a05bdc455bccdcf6acbb3
rajithst <rajiththennakoonshady@gmail.com>
2021-05-22 22:28:40 +0900

    linked-lists delete by value method and insert after node method implementation

--- linked_lists/singly_linked_list.py
+++ linked_lists/singly_linked_list.py
@@ -23,6 +23,30 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def insert_after_node(self, previous_node, data):
+        if not previous_node:
+            print("Previous Node doesn't exist")
+            return
+        new_node = Node(data)
+        new_node.next = previous_node.next
+        previous_node.next = new_node
+
+    def delete_by_value(self, value):
+        curr_node = self.head
+        if curr_node and curr_node.data == value:
+            self.head = curr_node.next
+            curr_node = None
+            return
+
+        previous_node = None
+        while curr_node is not None and curr_node.data != value:
+            previous_node = curr_node
+            curr_node = curr_node.next
+        if curr_node is None:
+            return
+        previous_node.next = curr_node.next
+        curr_node = None
+
     def print_list(self):
         curr_node = self.head
         while curr_node:
@@ -36,4 +60,6 @@ llist.append("B")
 llist.append("C")
 llist.append("D")
 llist.prepend("E")
+
+llist.insert_after_node(llist.head.next, "F")
 llist.print_list()
