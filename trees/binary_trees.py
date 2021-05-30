e674864d53c490f6edcf643cc4653f8224db748a
rajithst <rajiththennakoonshady@gmail.com>
2021-05-30 16:57:32 +0900

    binary-tree  tree traversal (in-order,pre-order,post-order) implementation

--- trees/binary_trees.py
+++ trees/binary_trees.py
@@ -9,10 +9,35 @@ class BinaryTree:
         self.root = Node(root)
 
     """
-    
+    Tree traversal - depth first
     """
+    def preorder_traversal(self,start,traversal):
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_traversal(start.left,traversal)
+            traversal = self.preorder_traversal(start.right,traversal)
+        return traversal
+
+    def inorder_traversal(self,start,traversal):
+        if start:
+            traversal = self.inorder_traversal(start.left,traversal)
+            traversal += (str(start.value) + "-")
+            traversal = self.inorder_traversal(start.right,traversal)
+        return traversal
+
+    def postorder_traversal(self,start,traversal):
+        if start:
+            traversal = self.postorder_traversal(start.left,traversal)
+            traversal = self.postorder_traversal(start.right,traversal)
+            traversal += (str(start.value) + "-")
+        return traversal
+
+
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
+print(tree.preorder_traversal(tree.root,""))
+print(tree.inorder_traversal(tree.root,""))
+print(tree.postorder_traversal(tree.root,""))
