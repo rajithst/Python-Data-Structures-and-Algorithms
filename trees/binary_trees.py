127c8007e2536338f43c946143bf9ad96008bca3
rajithst <rajiththennakoonshady@gmail.com>
2021-05-30 14:24:00 +0900

    binary-tree  initialize node and binary tree

---
+++ trees/binary_trees.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self,root):
+        self.root = Node(root)
+
+    """
+    
+    """
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
