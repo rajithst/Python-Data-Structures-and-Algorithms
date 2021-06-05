8d6bac5727af84f39cf093b2b1343d7c78e27e38
rajithst <rajiththennakoonshady@gmail.com>
2021-06-05 17:29:35 +0900

    circular-linked-list delete node by value implementation

--- linked_lists/circular_linked_list.py
+++ linked_lists/circular_linked_list.py
@@ -32,6 +32,27 @@ class CircularLinkedList:
             current_node.next = new_node
         self.head = new_node
 
+    def delete_node(self, data):
+        if self.head:
+            if self.head.data == data:
+                current_node = self.head
+                while current_node.next != self.head:
+                    current_node = current_node.next
+                if self.head == self.head.next:
+                    self.head = None
+                else:
+                    current_node.next = self.head.next
+                    self.head = self.head.next
+            else:
+                current_node = self.head
+                previous_node = None
+                while current_node.next != self.head:
+                    previous_node = current_node
+                    current_node = current_node.next
+                    if current_node.data == data:
+                        previous_node.next = current_node.next
+                        current_node = current_node.next
+
     def print_list(self):
         curr = self.head
         while curr:
@@ -46,4 +67,5 @@ cllist.append("C")
 cllist.append("D")
 cllist.prepend("B")
 cllist.prepend("A")
+cllist.delete_node("A")
 cllist.print_list()
