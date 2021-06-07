853e294d30fd726d5b1faf2e4f1b68ff40eaeabc
rajithst <rajiththennakoonshady@gmail.com>
2021-06-07 13:33:11 +0900

    doubly-linked-list add_node_after implementation

--- linked_lists/doubly_linked_list.py
+++ linked_lists/doubly_linked_list.py
@@ -31,6 +31,25 @@ class DoublyLinkedList:
             new_node.next = self.head
             self.head = new_node
 
+    def add_after_node(self,key,data):
+        current_node = self.head
+        while current_node:
+            if current_node.next is None and current_node.data == key:
+                self.append(data)
+                return
+            elif current_node.data == key:
+                new_node = Node(data)
+                next_node = current_node.next
+                previous_node = current_node.prev
+
+                current_node.next = new_node
+                new_node.next = next_node
+                new_node.prev = current_node
+                next_node.prev = new_node
+                return
+            current_node = current_node.next
+
+
     def print_list(self):
         cur = self.head
         while cur:
@@ -45,5 +64,8 @@ dllist.append(2)
 dllist.append(3)
 dllist.append(4)
 dllist.prepend(5)
+dllist.print_list()
+print("-"*10)
+dllist.add_after_node(2,110)
 
 dllist.print_list()
