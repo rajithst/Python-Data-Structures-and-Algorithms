49858a509612a5ad8e6fa5e7816ef0d0aa64d2d5
rajithst <rajiththennakoonshady@gmail.com>
2021-06-10 22:45:46 +0900

    binary-trees level-order-traversal implementation

--- trees/binary_trees.py
+++ trees/binary_trees.py
@@ -1,35 +1,59 @@
+from trees.queue import Queue
+
+
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
+
 class BinaryTree:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = Node(root)
 
     """
     Tree traversal - depth first
     """
-    def preorder_traversal(self,start,traversal):
+
+    def preorder_traversal(self, start, traversal):
         if start:
             traversal += (str(start.value) + "-")
-            traversal = self.preorder_traversal(start.left,traversal)
-            traversal = self.preorder_traversal(start.right,traversal)
+            traversal = self.preorder_traversal(start.left, traversal)
+            traversal = self.preorder_traversal(start.right, traversal)
         return traversal
 
-    def inorder_traversal(self,start,traversal):
+    def inorder_traversal(self, start, traversal):
         if start:
-            traversal = self.inorder_traversal(start.left,traversal)
+            traversal = self.inorder_traversal(start.left, traversal)
             traversal += (str(start.value) + "-")
-            traversal = self.inorder_traversal(start.right,traversal)
+            traversal = self.inorder_traversal(start.right, traversal)
         return traversal
 
-    def postorder_traversal(self,start,traversal):
+    def postorder_traversal(self, start, traversal):
         if start:
-            traversal = self.postorder_traversal(start.left,traversal)
-            traversal = self.postorder_traversal(start.right,traversal)
+            traversal = self.postorder_traversal(start.left, traversal)
+            traversal = self.postorder_traversal(start.right, traversal)
             traversal += (str(start.value) + "-")
+        return traversal
+
+    """
+    level order traversal
+    """
+
+    def level_order_traversal(self, start):
+        if start is None:
+            return
+        queue = Queue()
+        queue.enqueue(start)
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
         return traversal
 
 
@@ -38,6 +62,4 @@ tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
-print(tree.preorder_traversal(tree.root,""))
-print(tree.inorder_traversal(tree.root,""))
-print(tree.postorder_traversal(tree.root,""))
+print(tree.level_order_traversal(tree.root))
