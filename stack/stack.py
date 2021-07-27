941243c49517772b1bfe33c3dd25ce1aba946913
rajithst <rajiththennakoonshady@gmail.com>
2021-07-27 23:22:26 +0900

    stack core data-structure implementation

---
+++ stack/stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def get_stack(self):
+        return self.items
+
+
+myStack = Stack()
+myStack.push("A")
+myStack.push("B")
+myStack.push("C")
+myStack.push("D")
+print(myStack.peek())
