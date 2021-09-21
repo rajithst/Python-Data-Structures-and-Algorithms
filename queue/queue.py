013bea85850df1207f79d9edc78d362198d961d8
rajithst <rajiththennakoonshady@gmail.com>
2021-09-21 22:22:54 +0900

    Queue data structure implementation

---
+++ queue/queue.py
@@ -0,0 +1,29 @@
+class Queue:
+    def __init__(self):
+        self.queue_list = []
+
+    def is_empty(self):
+        return len(self.queue_list) == 0
+
+    def enqueue(self, val):
+        self.queue_list.insert(0, val)
+
+    def dequeue(self):
+        if self.is_empty() is False:
+            return self.queue_list.pop()
+        return None
+
+    def size(self):
+        if not self.is_empty():
+            return len(self.queue_list)
+        return 0
+
+    def front(self):
+        if not self.is_empty():
+            return self.queue_list[-1]
+        return None
+
+    def rear(self):
+        if not self.is_empty():
+            return self.queue_list[0]
+        return None
