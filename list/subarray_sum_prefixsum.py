6c4dc213ad13ade02ccf5ecf29437988480d8f78
rajithst <rajiththennakoonshady@gmail.com>
2021-11-16 21:57:00 +0900

    Arrays - largest subarray implementation using prefix sum

---
+++ list/subarray_sum_prefixsum.py
@@ -0,0 +1,18 @@
+arr_list = [-2, 3, 4, -1, 5, -12, 6, 1, 3]
+
+
+def largest_sub_array(arr):
+    prefix = [0]*len(arr)
+    prefix[0] = arr[0]
+    for k in range(1, len(arr)):
+        prefix[k] = prefix[k - 1] + arr[k]
+
+    max_sub_array = 0
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            sub_arr_sum = prefix[j] - prefix[i - 1] if i > 0 else prefix[j];
+            max_sub_array = max(max_sub_array, sub_arr_sum)
+    return max_sub_array
+
+
+print(largest_sub_array(arr_list))
